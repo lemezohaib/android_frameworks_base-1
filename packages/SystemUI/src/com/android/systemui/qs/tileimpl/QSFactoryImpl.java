23fc9d4e4d6e950c3827f50b00b42b23886c9bf8
Karan Parashar <karan@pixelos.net>
2023-01-02 20:37:52 +0000

    SystemUI: Make Bluetooth dialog tile optional
    Change-Id: I9474686e45c60b5e7dc81d874c7262324fdc68a0

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -33,6 +33,7 @@ import com.android.systemui.qs.tiles.AlarmTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
 import com.android.systemui.qs.tiles.AODTile;
 import com.android.systemui.qs.tiles.BatterySaverTile;
+import com.android.systemui.qs.tiles.BluetoothStockTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.CameraToggleTile;
@@ -86,6 +87,7 @@ public class QSFactoryImpl implements QSFactory {
 
     private final Provider<WifiTile> mWifiTileProvider;
     private final Provider<InternetTile> mInternetTileProvider;
+    private final Provider<BluetoothStockTile> mBluetoothStockTileProvider;
     private final Provider<BluetoothTile> mBluetoothTileProvider;
     private final Provider<CellularTile> mCellularTileProvider;
     private final Provider<DndTile> mDndTileProvider;
@@ -139,6 +141,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<CustomTile.Builder> customTileBuilderProvider,
             Provider<WifiTile> wifiTileProvider,
             Provider<InternetTile> internetTileProvider,
+            Provider<BluetoothStockTile> bluetoothStockTileProvider,
             Provider<BluetoothTile> bluetoothTileProvider,
             Provider<CellularTile> cellularTileProvider,
             Provider<DndTile> dndTileProvider,
@@ -187,6 +190,7 @@ public class QSFactoryImpl implements QSFactory {
 
         mWifiTileProvider = wifiTileProvider;
         mInternetTileProvider = internetTileProvider;
+        mBluetoothStockTileProvider = bluetoothStockTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
         mCellularTileProvider = cellularTileProvider;
         mDndTileProvider = dndTileProvider;
@@ -251,6 +255,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mWifiTileProvider.get();
             case "internet":
                 return mInternetTileProvider.get();
+            case "btstock":
+                return mBluetoothStockTileProvider.get();
             case "bt":
                 return mBluetoothTileProvider.get();
             case "cell":
